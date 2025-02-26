922eb9a60e494ed731f030fe0a98329f2254ccde
EduSelber <eduardosc6@al.insper.edu.br>
2025-02-26 09:55:19 -0300

    improve the order of inputs

--- roteiro1.py
+++ roteiro1.py
@@ -1,9 +1,8 @@
 import socket
 import sys
-import os
 from concurrent.futures import ThreadPoolExecutor
-from tqdm import tqdm 
-import ipaddress  # Novo módulo para manipulação de sub-redes
+from tqdm import tqdm
+import ipaddress  
 
 well_known_ports = {
     22: "SSH",
@@ -33,7 +32,6 @@ well_known_ports = {
 }
 
 def detect_os_from_banner(banner):
-    
     os_patterns = {
         'Linux': ['Ubuntu', 'Debian', 'Red Hat', 'CentOS', 'Fedora'],
         'Windows': ['Windows NT', 'Microsoft Windows', 'IIS'],
@@ -42,8 +40,6 @@ def detect_os_from_banner(banner):
         'Nginx': ['nginx'],
         'SSH': ['OpenSSH'],
     }
-
-    
     for os, patterns in os_patterns.items():
         for pattern in patterns:
             if pattern.lower() in banner.lower():
@@ -64,14 +60,14 @@ def banner_grab(ports, target, family):
             else:
                 print(f"Port {port} - No banner available")
             s.close()
-        except Exception as e:
+        except Exception:
             print(f"Port {port} - No banner available")
     return
+
 def scan_port(target, port, family):
     try:
         s = socket.socket(family, socket.SOCK_STREAM)
         s.settimeout(1)
-        
         result = s.connect_ex((target, port))  
         
         if result == 0:  
@@ -93,7 +89,7 @@ def scan_port(target, port, family):
         s.close()
         return (port, "N/A", "Filtered")  
 
-    except Exception as e:
+    except Exception:
         return None
 
 def scanning(start_port, end_port, target, family):
@@ -145,74 +141,71 @@ def scanning(start_port, end_port, target, family):
     open_ports = [port for port, _, status in port_status if status == "Open"]
     return open_ports
 
-
-
 def scan_network(network, start_port, end_port, family):
-    
     open_ports_per_host = {}
 
     for ip in ipaddress.IPv4Network(network, strict=False):
         target = str(ip)
-        print("\n")
-        print("-"*20)
-        print(f"Scanning host with IP:{target} ")
+        print("\n" + "-"*20)
+        print(f"Scanning host with IP: {target}")
         open_ports = scanning(start_port, end_port, target, family)
         if open_ports:
             open_ports_per_host[target] = open_ports
         print("-"*20)
-        
     
     return open_ports_per_host
 
 def main():
-    target = input("Enter the IP(Ipv4 or Ipv6) address or hostname of the target server/host:  ")
-    family = None
-    target_ip = None
-
-    try:
-        addr_info = socket.getaddrinfo(target, None)
-        target_ip = addr_info[0][4][0]
-        family = addr_info[0][0]
-        print(f"Resolving host {target} to IP {target_ip}")
-    except socket.gaierror:
-        print("Invalid host or IP address")
-        sys.exit(1)
-
-    try:
-        start_port = int(input("Enter the starting port: "))
-        end_port = int(input("Enter the ending port: "))
-    except ValueError:
-        print("Invalid input. Please enter numeric values for the ports.")
-        sys.exit(1)
-
     choice = input("Would you like to scan a single host or a network? (single/network): ").lower()
     
-    if choice == 'single':
+    if choice == 'network':
+        network_ip = input("Enter the network IP: ")
+        subnet_mask = input("Enter the subnet mask (between 0 and 32): ")
+        try:
+            if not (0 <= int(subnet_mask) <= 32):
+                print("Invalid subnet mask. Please enter a value between 0 and 32.")
+                sys.exit(1)
+        except ValueError:
+            print("Invalid subnet mask. Please enter a numeric value.")
+            sys.exit(1)
+
+        network = f"{network_ip}/{subnet_mask}"
+        start_port = int(input("Enter the starting port: "))
+        end_port = int(input("Enter the ending port: "))
+        
+        print(f'Scanning network: {network} from port {start_port} to {end_port}')
+        open_ports_per_host = scan_network(network, start_port, end_port, socket.AF_INET)
+        
+        for host, open_ports in open_ports_per_host.items():
+            print("\n" + "-"*20)
+            print(f"Host {host} has open ports: {open_ports}")
+            banner_grab(open_ports, host, socket.AF_INET)
+            print("-"*20)
+    
+    elif choice == 'single':
+        target = input("Enter the IP(Ipv4 or Ipv6) address or hostname of the target server/host:  ")
+        
+        try:
+            addr_info = socket.getaddrinfo(target, None)
+            target_ip = addr_info[0][4][0]
+            family = addr_info[0][0]
+            print(f"Resolving host {target} to IP {target_ip}")
+        except socket.gaierror:
+            print("Invalid host or IP address")
+            sys.exit(1)
+
+        start_port = int(input("Enter the starting port: "))
+        end_port = int(input("Enter the ending port: "))
+
         print(f'Scanning host with IP: {target_ip} from port {start_port} to {end_port}')
         open_ports = scanning(start_port, end_port, target_ip, family)
         banner_grab(open_ports, target_ip, family)
-    elif choice == 'network':
-        print(f'Scanning network: {target_ip} from port {start_port} to {end_port}')
-        subnet_mask = input("Enter the subnet mask (between 0 and 32): ")
-        
-        if not (0 <= int(subnet_mask) <= 32):
-            print("Invalid subnet mask. Please enter a value between 0 and 32.")
-            sys.exit(1)
-
-        network = f"{target_ip}/{subnet_mask}"
-        open_ports_per_host = scan_network(network, start_port, end_port, family)
-        
-        for host, open_ports in open_ports_per_host.items():
-            print("\n")
-            print("-"*20)
-            print(f"Host {host} has open ports: {open_ports}")
-            banner_grab(open_ports, host, family)
-            print("-"*20)
+    
     else:
         print("Invalid choice.")
     
     print("Scan completed.")
     sys.exit(0)
-    
+
 if __name__ == "__main__":
     main()
